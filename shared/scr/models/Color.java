3293f72fa53203125d05ed5c63230506ed926a30
Scriptyyy <scriptyyy@mail.ru>
2026-04-23 00:09:42 +0300

    feat: пакет models добавлен в shared

---
+++ shared/scr/models/Color.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Перечисление возможных цветов дракона.
+ */
+public enum Color implements Serializable {
+    GREEN,
+    RED,
+    BLACK,
+    BLUE,
+    YELLOW;
+}
---
+++ shared/scr/models/Country.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Перечисление возможных национальностей.
+ */
+public enum Country implements Serializable {
+    CHINA,
+    INDIA,
+    THAILAND,
+    NORTH_KOREA;
+}
