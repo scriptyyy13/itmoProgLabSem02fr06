6c7d3fbe9d792f75725087d4d9e8d49629dc549d
Scriptyyy <scriptyyy@mail.ru>
2026-04-27 21:09:39 +0300

    Заготовка под основной класс клиента

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,7 +1,24 @@
-public class Main {
-    public static void main() {
-        ClientCommandManager clientCommandManager = new ClientCommandManager(new Reader());
-        clientCommandManager.start();
-    }
+import exceptions.InvalidInputException;
+import java.util.NoSuchElementException;
 
+public class Main {
+    public static void main(String[] args) {
+        Reader consoleReader = new Reader();
+        OutputManager.println("Добро пожаловать в клиентское приложение!");
+
+        /* тут для сети */
+
+        ClientCommandManager commandManager = new ClientCommandManager(consoleReader);
+
+        try {
+            OutputManager.println("Введите команду (или 'help' для списка доступных команд):");
+            commandManager.start();
+        } catch (NoSuchElementException e) {
+            OutputManager.errPrintln("\nЗавершение работы клиента.");
+        } catch (Exception e) {
+            OutputManager.errPrintln("Критическая ошибка в работе клиента: " + e.getMessage());
+        } finally {
+            OutputManager.println("Клиент остановлен.");
+        }
+    }
 }
