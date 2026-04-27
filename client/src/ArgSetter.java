2260a779c2446fdc1362683fab0581d84cd67997
Scriptyyy <scriptyyy@mail.ru>
2026-04-27 21:07:25 +0300

    Переписана установка аргументов для лучшей читаемости

--- client/src/ArgSetter.java
+++ client/src/ArgSetter.java
@@ -10,29 +10,22 @@ import java.nio.file.Paths;
 public class ArgSetter {
 
     public static void setArgs(Command command, Arg[] args, Reader reader) {
-        switch (command) {
-            case Add a:
-                command.setArgs(new Arg(InputManager.inputDragon(reader)));
-                break;
-            case AddIfMax amx:
-                command.setArgs(new Arg(InputManager.inputDragon(reader)));
-                break;
-            case AddIfMin amn:
-                command.setArgs(new Arg(InputManager.inputDragon(reader)));
-                break;
-            case Update u:
-                command.setArgs(args[0], new Arg(InputManager.inputDragon(reader)));
-                break;
-            case ExecuteScript ex:
-                try {
-                    command.setArgs(new Arg(Files.readString(Paths.get((String)args[0].getValue()))));
-                } catch (IOException e) {
-                    throw new InvalidInputException("Файл не найден");
-                }
-                break;
-            default:
-                command.setArgs(args);
-                break;
+        if (command instanceof Add || command instanceof AddIfMax || command instanceof AddIfMin) {
+            command.setArgs(new Arg(InputManager.inputDragon(reader)));
+        }
+        else if (command instanceof Update) {
+            command.setArgs(args[0], new Arg(InputManager.inputDragon(reader)));
+        }
+        else if (command instanceof ExecuteScript) {
+            try {
+                String path = (String) args[0].getValue();
+                command.setArgs(new Arg(Files.readString(Paths.get(path))));
+            } catch (IOException e) {
+                throw new InvalidInputException("Файл не найден");
+            }
+        }
+        else {
+            command.setArgs(args);
         }
     }
 }
