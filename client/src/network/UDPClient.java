83eab7ec2e5813f00bb8db2d954b17384979b783
Scriptyyy <scriptyyy@mail.ru>
2026-04-28 22:32:32 +0300

    Класс работы с сетью

---
+++ client/src/network/UDPClient.java
@@ -0,0 +1,50 @@
+package network;
+
+import commands.Command;
+import tools.Message;
+
+import java.io.*;
+import java.net.*;
+
+public class UDPClient {
+    private final DatagramSocket socket;
+    private final InetAddress serverAddress;
+    private final int serverPort;
+    private final int TIMEOUT = 5000; // таймаут в 5 секунд
+
+    public UDPClient(String host, int port) throws SocketException, UnknownHostException {
+        this.socket = new DatagramSocket();
+        this.socket.setSoTimeout(TIMEOUT);
+        this.serverAddress = InetAddress.getByName(host);
+        this.serverPort = port;
+    }
+
+    /** Отправка команды в виде байтового объекта */
+    public void sendCommand(Command command) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(command); // Отправляем саму команду со всеми аргументами
+        oos.flush();
+        byte[] data = baos.toByteArray();
+
+        DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, serverPort);
+        socket.send(packet);
+    }
+
+    /** Получение сообщения от сервера */
+    public Message receiveResponse() throws IOException, ClassNotFoundException {
+        byte[] buffer = new byte[65535]; // максимальный размер пакета
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return (Message) ois.readObject();
+    }
+
+    public void close() {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
+    }
+}
