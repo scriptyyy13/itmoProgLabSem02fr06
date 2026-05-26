3160ad28bc2ac78980bd393d2aa9260bec16cea3
Scriptyyy <scriptyyy@mail.ru>
2026-05-26 21:06:15 +0300

    Команда для авторизации пользователя

---
+++ server/src/serverCommands/Login.java
@@ -0,0 +1,40 @@
+package serverCommands;
+
+import commands.CommandRequest;
+import database.DatabaseManager;
+import sharedTools.Arg;
+import serverTools.CollectionManager;
+
+/**
+ * Команда для авторизации пользователя.
+ */
+public class Login extends Command {
+
+    public Login(CommandRequest commandRequest, CollectionManager collectionManager) {
+        super(commandRequest, collectionManager);
+        this.requiresAuth = false;
+    }
+
+    /**
+     * Исполнение команды авторизации.
+     */
+    @Override
+    public String execute() {
+        Arg[] args = getArgs();
+        if (args == null || args.length < 2) {
+            return "Ошибка: Неверное количество аргументов для входа.";
+        }
+
+        String login = args[0].toString();
+        String password = args[1].toString();
+
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+        long userId = dbManager.validateUser(login, password);
+
+        if (userId != -1) {
+            return "SUCCESS_LOGIN:" + userId;
+        } else {
+            return "Ошибка: Неверный логин или пароль.";
+        }
+    }
+}
