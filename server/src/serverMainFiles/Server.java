ee11b3e3facf6d991675e01a3caebff332b5891d
Scriptyyy <scriptyyy@mail.ru>
2026-05-26 19:09:21 +0300

    Использование базы данных вместо файла

--- server/src/serverMainFiles/Server.java
+++ server/src/serverMainFiles/Server.java
@@ -1,12 +1,13 @@
 package serverMainFiles;
 
+import database.DatabaseManager;
 import serverExceptions.XmlReadingException;
 import models.Dragon;
 import serverTools.CollectionManager;
 import serverTools.ConfigManager;
 import serverTools.ServerCommandManager;
-import serverTools.XMLReader;
 
+import java.sql.SQLException;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
@@ -16,16 +17,21 @@ public class Server {
     public static void main(String[] args) {
         if (args.length > 0) {
             ConfigManager.scanConfig(args[0]);
+        } else {
+            ConfigManager.scanConfig("server.preps");
         }
-        ApplicationContext.collectionPath = ConfigManager.collectionFile;
+
         ConcurrentLinkedDeque<Dragon> collection = new ConcurrentLinkedDeque<Dragon>();
         try {
-
-            collection = XMLReader.readXmlCollection(ApplicationContext.collectionPath);
+            DatabaseManager db = DatabaseManager.getInstance();
+            collection = db.fetchAllDragons();
         } catch (XmlReadingException e) {
             System.out.println(e.getMessage());
             System.out.println("Коллекция пуста");
+        } catch (SQLException e) {
+            System.err.println("Ошибка инициализации БД: " + e.getMessage());
         }
+
         CollectionManager collectionManager = new CollectionManager(collection);
         ServerCommandManager commandManager = new ServerCommandManager(ConfigManager.port, collectionManager);
         commandManager.start();
