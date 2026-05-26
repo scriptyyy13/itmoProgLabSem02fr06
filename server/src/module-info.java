9586b20e5c8e5f243473ce7d1f0d23c2c678b055
Scriptyyy <scriptyyy@mail.ru>
2026-05-26 22:05:13 +0300

    Подключения и мелкие исправления

--- server/src/module-info.java
+++ server/src/module-info.java
@@ -5,6 +5,8 @@ module server {
     requires shared;
     requires com.fasterxml.jackson.dataformat.xml;
     requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires java.sql;
 
     exports serverMainFiles;
     exports serverCommands;
