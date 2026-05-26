0db31508ad3bb8efa5168074bb7ff17a3a800016
Scriptyyy <scriptyyy@mail.ru>
2026-05-26 15:46:48 +0300

    Инициализация таблиц бд

---
+++ server/src/database/init.sql
@@ -0,0 +1,37 @@
+-- Таблица пользователей
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(56) NOT NULL -- SHA-224 (HEX)
+);
+
+-- Таблица драконов (вложенные поля храним в одной строке, чтобы не плодить потом join'ы)
+CREATE TABLE IF NOT EXISTS dragons (
+      id SERIAL PRIMARY KEY,
+      creator_id INT NOT NULL,
+      name TEXT NOT NULL,
+
+    -- Coordinates
+      coord_x REAL NOT NULL,
+      coord_y DOUBLE PRECISION NOT NULL,
+
+      creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      age BIGINT NOT NULL,
+      weight INT,
+      speaking BOOLEAN NOT NULL,
+      color VARCHAR(20),
+
+    -- Person (Killer)
+      killer_name TEXT,
+      killer_birthday DATE,
+      killer_passport_id TEXT,
+      killer_nationality VARCHAR(20),
+
+    -- Location (внутри Killer)
+      killer_loc_x INT,
+      killer_loc_y INT,
+      killer_loc_z INT,
+      killer_loc_name TEXT,
+
+      FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+);
