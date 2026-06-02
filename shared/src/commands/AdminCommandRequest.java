8c592414241c2270db3f4b75555d12511be2d697
Scriptyyy <scriptyyy@mail.ru>
2026-06-03 00:07:15 +0300

    Основной класс команд для перехвата и обработки их балансером

---
+++ shared/src/commands/AdminCommandRequest.java
@@ -0,0 +1,12 @@
+package commands;
+
+/**
+ * Базовый класс для команд управления балансером.
+ */
+public abstract class AdminCommandRequest extends CommandRequest {
+    /**
+     * Создание экземпляра команды {@code add_server}.
+     */
+    public AdminCommandRequest() {
+    }
+}
