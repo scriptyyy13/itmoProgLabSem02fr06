acb4e65104941212f639cc7c94d702d3702d0080
Scriptyyy <scriptyyy@mail.ru>
2026-06-02 20:02:20 +0300

    Исключения для токенов

---
+++ shared/src/exceptions/TokenException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class TokenException extends Exception {
+    public TokenException(String message) {
+        super(message);
+    }
+
+    public TokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
